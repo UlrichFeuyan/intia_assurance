d302ef3b10554275a60a4dce23f060af3ac2fd64
UlrichFeuyan <ulrich.feuyan@gmail.com>
2026-02-18 14:07:49 +0100

    Ajoute le modÃ¨le Agency

--- backend/agencies/models.py
+++ backend/agencies/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Agency(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    city = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name = "Agency"
+        verbose_name_plural = "Agencies"
+
+    def __str__(self) -> str:
+        return self.name
